2e4d23438811d765b1c4cddfaf8ae20c1e6a432f
Abdelhalim El Ouardy <abdeel-o@c1r7p1.1337.ma>
2023-06-08 12:13:14 +0100

    expand wild card inside vars

--- SRCS/assist/helper.c
+++ SRCS/assist/helper.c
@@ -6,7 +6,7 @@
 /*   By: abdeel-o < abdeel-o@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 13:11:01 by hmeftah           #+#    #+#             */
-/*   Updated: 2023/06/06 18:43:50 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/06/07 19:24:59 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,49 @@ char	*x_join(char **tab)
 	return (ft_strtrim(_x, "\x01"));
 }
 
+int _strlen(char *s, char c)
+{
+	int	l;
+
+	l = 0;
+	while (s[l] && s[l] != c)
+		l++;
+	return (l);
+}
+
+void	_retrieve(char **exp, char *v)
+{
+	char	*pattern;
+	t_node	*node;
+
+	pattern = ft_substr(v, 0, _strlen(v, ' '));
+	node = ft_lstnew(pattern, WILD);
+	if (!ft_strcmp(pattern, "$?"))
+		node->value = ft_itoa(g_gen.e_status);
+	else
+		wildcard_handler(node);
+	*exp = ft_strjoin(*exp, node->value);
+}
+
+char	*expand_wild(char *v)
+{
+	char	*exp;
+
+	exp = "";
+	while (*v)
+	{
+		if (*v == '*' && *(v - 1) == ' ')
+		{
+			_retrieve(&exp, v);
+			v += _strlen(v, ' ') - 1;
+		}
+		else
+			exp = ft_strjoin(exp, fry_char_to_str(*v));
+		v++;
+	}
+	return (exp);
+}
+
 char	*get_env_value(char *name, t_env *env, int herdoc)
 {
 	t_env	*found;
@@ -49,14 +92,21 @@ char	*get_env_value(char *name, t_env *env, int herdoc)
 	found = get_env(name, env);
 	if (found)
 	{
+		if (ft_strchr(found->value, '*') && !herdoc)
+		 	return (expand_wild(found->value));
 		if (!herdoc)
 		{
 			split = ft_split(found->value, ' ');
-			return (x_join(split));
+		 	return (x_join(split));
 		}
-		return (found->value);
+		else
+			return (found->value);
 	}
 	else
 		return (NULL);
 	return (NULL);
 }
+
+
+//export A="*a"
+//echo $A
