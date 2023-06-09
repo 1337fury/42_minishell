877c58db6aaee12cf4e86534a8f0c47afc5938d0
Hicham Meftah <hmeftah@c1r6p3.1337.ma>
2023-06-09 15:12:41 +0100

    Final echo

--- SRCS/builtins/echo.c
+++ SRCS/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: hmeftah <hmeftah@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 20:06:23 by hmeftah           #+#    #+#             */
-/*   Updated: 2023/06/02 16:25:33 by hmeftah          ###   ########.fr       */
+/*   Updated: 2023/06/09 15:11:11 by hmeftah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,34 +26,46 @@ static void	print_with_newline(t_env *home, char **arg)
 			ft_putstr_fd(arg[i] + 1, 1);
 		}
 		else
-		{
 			ft_putstr_fd(arg[i], 1);
+		if (arg[i + 1])
 			ft_putchar_fd(' ', 1);
-		}	
 		i++;
 	}
-	printf("\n");
+	ft_putchar_fd('\n', 1);
+}
+
+bool	dumb_string(char *arg)
+{
+	int	i;
+
+	i = 1;
+	while (arg[++i])
+	{
+		if (arg[i] != 'n')
+			return (false);
+	}
+	return (true);
 }
 
 static void	print_without_newline(t_env *home, char **arg)
 {
 	int	i;
 
-	i = 2;
-	while (arg[i])
+	i = 1;
+	while (arg[++i])
 	{
+		if ((arg[i][0] == '-' && arg[i][1] == 'n' && arg[i][2] == '\0')
+			|| dumb_string(arg[i]))
+			continue ;
 		if (!ft_strcmp("~/", arg[i]) || !ft_strcmp("~", arg[i]))
 		{
 			ft_putstr_fd(home->value, 1);
 			ft_putstr_fd(arg[i] + 1, 1);
-			ft_putchar_fd(' ', 1);
 		}
 		else
-		{
 			ft_putstr_fd(arg[i], 1);
+		if (arg[i + 1])
 			ft_putchar_fd(' ', 1);
-		}
-		i++;
 	}
 }
 
@@ -70,9 +82,10 @@ void	_echo(t_general *g_master, char **arg)
 	}
 	else
 	{
-		if (!ft_strcmp(arg[1], "-n"))
+		if (arg[1] && !ft_strncmp(arg[1], "-n", ft_strlen("-n"))
+			&& dumb_string(arg[1]))
 			print_without_newline(home, arg);
-		else if (ft_strcmp(arg[1], "-n"))
+		else if (arg[1] && ft_strcmp(arg[1], "-n"))
 			print_with_newline(home, arg);
 	}
 	builtins_exit(g_master, 0);
